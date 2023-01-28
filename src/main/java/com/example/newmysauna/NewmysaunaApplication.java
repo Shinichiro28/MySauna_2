db3a2107bcb76912c7deab15ec6906b743ebed73
shinichiro yoshida <s.erk.ikt@gmail.com>
2023-01-29 00:56:38 +0900

    First Committ

---
+++ src/main/java/com/example/newmysauna/NewmysaunaApplication.java
@@ -0,0 +1,13 @@
+package com.example.newmysauna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewmysaunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewmysaunaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/newmysauna/NewmysaunaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newmysauna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewmysaunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
